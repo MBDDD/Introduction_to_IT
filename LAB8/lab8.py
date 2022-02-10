59c957aa93dc56e4fa0b6a781df3c96637f82e9b
mbdd <sl_mc1@mail.ru>
2022-02-11 01:03:06 +0300

    update

--- LAB8/lab8.py
+++ LAB8/lab8.py
@@ -1,31 +1,12 @@
 import psycopg2
 import sys
-from PyQt5.QtWidgets import (QApplication, QWidget,
-                             QTabWidget, QAbstractScrollArea,
-                             QVBoxLayout, QHBoxLayout,
-                             QTableWidget, QGroupBox,
-                             QTableWidgetItem, QPushButton, QMessageBox)
+from PyQt5.QtWidgets import (QApplication, QWidget, QTabWidget, QAbstractScrollArea,
+                             QVBoxLayout, QHBoxLayout, QTableWidget, QGroupBox, QTableWidgetItem)
 
 
 class MainWindow(QWidget):
-    def create_shedule_tab(self):
-        self.shedule_tab = QWidget()
-        self.tabs.addTab(self.shedule_tab, 'Shedule')
-        self.monday_gbox = QGroupBox('Monday')
-        self.svbox = QVBoxLayout()
-        self.shbox1 = QHBoxLayout()
-        self.shbox2 = QHBoxLayout()
-        self.svbox.addLayout(self.shbox1)
-        self.svbox.addLayout(self.shbox2)
-        self.shbox1.addWidget(self.monday_gbox)
-        self._create_monday_table()
-        self.update_shedule_button = QPushButton('Update')
-        self.shbox2.addWidget(self.update_shedule_button)
-        self.update_shedule_button.clicked.connect(self._update_shedule)
-        self.shedule_tab.setLayout(self.svbox)
-
     def connect_to_db(self):
-        self.conn = psycopg2.connect(database='week',
+        self.conn = psycopg2.connect(database='lab8',
                                      user='postgres',
                                      password='mbdd',
                                      host='localhost',
@@ -34,51 +15,80 @@ class MainWindow(QWidget):
 
     def __init__(self):
         super(MainWindow, self).__init__()
+        # Название окна
         self.setWindowTitle('Shedule')
+        # Подгоняем размеры окна
         self.vbox = QVBoxLayout(self)
-        self.tabs = QTabWidget(self)
+        self.tabs = QTabWidget(self)  # Добавляем таблицу
         self.vbox.addWidget(self.tabs)
+
+        # Подключаемся к БД
         self.connect_to_db()
-        self.create_shedule_tab()
 
-    def _create_monday_table(self):
-        self.monday_table = QTableWidget()
-        self.monday_table.setSizeAdjustPolicy(QAbstractScrollArea.AdjustToContents)
-        self.monday_table.setColumnCount(3)
-        self.monday_table.setHorizontalHeaderLabels(['Subject', 'Time', 'Link'])
-        self._update_monday_table()
+        # Запрос на подсчёт количества схем в БД
+        self.cursor.execute('SELECT schema_name FROM information_schema.schemata')
+        info_schema = list(self.cursor.fetchall())
+        schema = []
+        for i in range(6, len(info_schema)):
+            schema.append(info_schema[i][0])
+        for i in range(len(schema)):  # от 1 до 2
+            # Создание вкладки и присваивание ей название
+            self.shedule_tab = QWidget()
+            self.tabs.addTab(self.shedule_tab, schema[i])
+            # Запрос на подсчёт количества таблиц в схеме
+            self.cursor.execute(f"SELECT * FROM information_schema.tables WHERE table_schema = '{schema[i]}'")
+            record = list(self.cursor.fetchall())
+            table = []
+            for n in range(len(record)):
+                table.append(record[n][2])
+            for j in range(len(record)):  # от 1 до 2
+                self.create_shedule_tab(schema[i], table[j])
+
+    def create_shedule_tab(self, schema, table):
+        # Название подвкладки
+        self.gbox = QGroupBox(table)
+        self.svbox = QVBoxLayout()
+        self.shbox1 = QHBoxLayout()
+        self.shbox2 = QHBoxLayout()
+        self.svbox.addLayout(self.shbox1)
+        self.svbox.addLayout(self.shbox2)
+
+        self.shbox1.addWidget(self.gbox)
+        self.create_table(schema, table)
+        self.shedule_tab.setLayout(self.svbox)
+        # # Добавляем кнопки для обработки (Сохранить)
+        # self.update_shedule_button = QPushButton('save')
+        # self.shbox2.addWidget(self.update_shedule_button)
+        # self.update_shedule_button.clicked.connect(self.update_table)
+
+    def create_table(self, schema, table):
+        self.table = QTableWidget()
+        self.table.setSizeAdjustPolicy(QAbstractScrollArea.AdjustToContents)
+
+        # Запрос на получение названий столбцов
+        self.cursor.execute(f"SELECT column_name FROM information_schema.columns WHERE table_name = '{table}'")
+        info = list(self.cursor.fetchall())
+        column = []
+        for i in range(len(info)):
+            column.append(info[i][0])
+
+        self.table.setColumnCount(len(column))
+        self.table.setHorizontalHeaderLabels(column)
+        self.update_table(schema, table)
         self.mvbox = QVBoxLayout()
-        self.mvbox.addWidget(self.monday_table)
-        self.monday_gbox.setLayout(self.mvbox)
+        self.mvbox.addWidget(self.table)
+        self.gbox.setLayout(self.mvbox)
 
-    def _update_shedule(self):
-        self._update_monday_table()
-
-    def _update_monday_table(self):
-        self.cursor.execute("SELECT * FROM timetable WHERE day='wednesday'")
-        records = list(self.cursor.fetchall())
-        self.monday_table.setRowCount(len(records)+1)
-        for i, r in enumerate(records):
-            r = list(r)
-            joinButton = QPushButton("Join")
-            self.monday_table.setItem(i, 0,QTableWidgetItem(str(r[1])))
-            self.monday_table.setItem(i, 1,QTableWidgetItem(str(r[2])))
-            self.monday_table.setCellWidget(i, 2, joinButton)
-            joinButton.clicked.connect(lambda ch, num=i:self._change_day_from_table(num))
-        self.monday_table.resizeRowsToContents()
-
-    def _change_day_from_table(self, rowNum):
-        row = list()
-        for i in range(self.monday_table.columnCount()):
-            try:
-                row.append(self.monday_table.item(rowNum, i).text())
-            except:
-                row.append(None)
-            try:
-                self.cursor.execute("UPDATE SQL запрос на изменение одной строки в базе данных", (row[0],))
-                self.conn.commit()
-            except:
-                QMessageBox.about(self, "Error", "Enter all fields")
+    def update_table(self, schema, table):
+        # Запрос на данные для заполнения в ячейки таблицы
+        self.cursor.execute(f"SELECT * FROM {schema}.{table}")
+        info = list(self.cursor.fetchall())
+        # Количество строчек таблицы
+        self.table.setRowCount(len(info))
+        for i in range(len(info)):
+            for j in range(len(info[0])):
+                self.table.setItem(i, j, QTableWidgetItem(info[i][j]))
+        self.table.resizeRowsToContents()
 
 
 app = QApplication(sys.argv)
